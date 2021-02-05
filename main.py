fa925a8afb341a1f6e235a407f10b04c63524682
Davern <davidlutman@telus.net>
2021-02-05 12:55:48 -0800

    added function stubs

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ import sys
 import tkinter as tk
 import random
 import pygame
+
+class image():
+    def __init__():
+        pass
+
+
+def drawWindow():
+    pass
+
+def redrawWindow():
+    pass
+
+def reset():
+    pass
+
+def main():
+    pass
+
+main()
