33a16e69fb0326bda0e246e7b74298df1f0ca8d2
Davern <51719544+Davern@users.noreply.github.com>
2021-02-09 13:36:34 -0800

    changed the images of fruits and implemented the Game class mostly

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import sys
-import tkinter as tk
-import random
+from random import *
 import pygame
 
 
 
 class Tile(object):
-    blankImage = pygame.image.load('apple.jpg')
-    def __init__(self, x, y, image, cover):
+    blankImage = pygame.image.load('whitesquare.png')
+    def __init__(self, tilePos, image, cover):
         self.image = image
         self.cover = cover
         self.covered = True
         self.width = self.image.get_width()
         self.height = self.image.get_height()
-        self.x = x
-        self.y = y
+        self.x = tilePos[0]
+        self.y = tilePos[1]
+        self.width = tilePos[2]
+        self.height = tilePos[3]
 
     def draw(self, image=blankImage):
         self.cover.blit(image, (self.x, self.y))
@@ -30,17 +31,46 @@ class Tile(object):
                     self.draw(self.image)
                     self.covered = False
 
-class Game():
-    def __init__(self):
-        pass
+class Game:
+    boardSize = 4
+    gap = 80
+    def __init__(self, surface, images):
+        self.gameOver = False
+        self.surface = surface
+        self.images = images
+        tileWidth = images[0].get_width()
+        tileHeight = images[0].get_height()
+
+        self.board = []
+        for row in range(0, Game.boardSize):
+            rows = []
+            for column in range(0, Game.boardSize):
+                x = ((column + 1)* Game.gap) + (column * tileWidth)
+                y = ((row + 1) * Game.gap) + (row * tileHeight)
+                tilePos = [x,y,tileWidth,tileHeight]
+
+                imageIndex = randrange(0, len(self.images))
+                image = self.images[imageIndex]
+                self.images.remove(image)
+                
+                print(len(self.images))
+                tile = Tile(tilePos,image,surface)
+
+                rows.append(tile)
+
+            self.board.append(rows)
 
     def draw(self):
+        for row in self.board:
+            for tile in row:
+                tile.draw()
+        
+        pygame.display.flip()
+
+    def handleMouseClick(self):
         pass
 
-    def handleMouseClick():
-        pass
-
-    def checkGameStatus():
+    def checkGameStatus(self):
         pass
 
 def main():
@@ -50,11 +80,35 @@ def main():
     window = pygame.display.set_mode((size,size))
     pygame.display.set_caption('Memory Challenge')
     flag = True
-    apple_image = pygame.image.load('apple.jpg')
-    orange_image = pygame.image.load('orange.jpg')
-    banana_image = pygame.image.load('banana.jpg')
+    images = []
+    apple_image = pygame.image.load('apple.png')
+    orange_image = pygame.image.load('orange.png')
+    banana_image = pygame.image.load('banana.png')
+    grape_image = pygame.image.load('grape.png')
+    images.append(apple_image)
+    images.append(apple_image)
+    images.append(orange_image)
+    images.append(orange_image)
+    images.append(banana_image)
+    images.append(banana_image)
+    images.append(grape_image)
+    images.append(grape_image)
+    images.append(apple_image)
+    images.append(apple_image)
+    images.append(orange_image)
+    images.append(orange_image)
+    images.append(banana_image)
+    images.append(banana_image)
+    images.append(grape_image)
+    images.append(grape_image)
+
+    print(len(images))
 
     clock = pygame.time.Clock()
+
+    instance = Game(window, images)
+    instance.draw()
+    pygame.display.update()
 
 
     while flag:
@@ -64,6 +118,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        redrawWindow(window)
+
+        pygame.display.update()
 
 main()
